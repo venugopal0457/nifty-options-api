c18f45b753c33c9573ac75127113af8eef188261
venugopal0457 <ivenu2323@gmail.com>
2026-03-10 23:55:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
+from flask import Flask, jsonify
+import requests
 
+app = Flask(__name__)
+
+@app.route("/nifty")
+
+def nifty():
+
+    url = "https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(url, headers=headers)
+    data = r.json()
+
+    records = data["records"]["data"]
+
+    max_call = 0
+    max_put = 0
+    call_strike = 0
+    put_strike = 0
+
+    for r in records:
+
+        if "CE" in r and r["CE"]["openInterest"] > max_call:
+            max_call = r["CE"]["openInterest"]
+            call_strike = r["strikePrice"]
+
+        if "PE" in r and r["PE"]["openInterest"] > max_put:
+            max_put = r["PE"]["openInterest"]
+            put_strike = r["strikePrice"]
+
+    bias = "BULLISH" if put_strike > call_strike else "BEARISH"
+
+    trade = f"BUY {put_strike} PE" if bias=="BEARISH" else f"BUY {call_strike} CE"
+
+    return jsonify({
+        "support": put_strike,
+        "resistance": call_strike,
+        "bias": bias,
+        "trade": trade
+    })
+
+app.run(host="0.0.0.0", port=10000)
